3bf66faea6e2889d9267631e26c9f9114b16a9b1
Matthew Giarra <matthew.giarra@jhuapl.edu>
2020-12-07 17:34:05 -0500

    Removed my username from paths

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,3 +1,3 @@
-DATA_DIR=$HOME/data
+DATA_DIR=$(pwd)/videos
 MODELS_DIR=$(pwd)/models
 docker run --rm -it -v $(pwd):/workspace -v $DATA_DIR:/data -v $MODELS_DIR:/models --gpus all trimmer ./build/trimmer config/trimmer_config.json
