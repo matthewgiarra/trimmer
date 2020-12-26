50e308e7fe0a097a64bf16cb253c2e602f84b7cd
Matthew Giarra <matthew.giarra@gmail.com>
2020-12-26 02:34:46 +0000

    Added dockerignore and added target argument to build_source.sh

--- scripts/build_source.sh
+++ scripts/build_source.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker run --rm -it -v $(pwd):/workspace --gpus all trimmer /bin/bash -c "mkdir -p /workspace/build && cd /workspace/build && cmake .. && make trimmer -j12"
+
+if [ "$#" -eq 0 ]; then
+    TARGET="trimmer"
+else
+    TARGET=$1
+fi
+
+docker run --rm -it -v $(pwd):/workspace --gpus all trimmer /bin/bash -c "mkdir -p /workspace/build && cd /workspace/build && cmake .. && make $TARGET -j12"
